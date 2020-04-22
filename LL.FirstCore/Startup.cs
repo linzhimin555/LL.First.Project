f38f4c9282362b4227af41fd57597b0bcaffcd07
Linlei123 <1061203603@qq.com>
2020-04-22 09:26:03 +0800

    [Add]简单swagger功能添加

--- LL.FirstCore/Startup.cs
+++ LL.FirstCore/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace LL.FirstCore
 {
@@ -25,6 +27,28 @@ namespace LL.FirstCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var basePath = AppContext.BaseDirectory;
+            #region using Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = $"LL.FirstCore 接口文档--NetCore 3.1",
+                    Description = "LL.FirstCore Api",
+                    Contact = new OpenApiContact { Name = "LL.FirstCore", Email = "1137020867@qq.com", Url = new Uri("https://github.com/linzhimin555/LL.First.Project") },
+                    License = new OpenApiLicense
+                    {
+                        Name = $"LL.FirstCore 官方文档",
+                        Url = new Uri("https://github.com/linzhimin555/LL.First.Project/blob/master/README.md")
+                    }
+                });
+                //就是这里
+                var xmlPath = Path.Combine(basePath, "LL.FirstCore.xml");//这个就是刚刚配置的xml文件名
+                c.IncludeXmlComments(xmlPath, true);//默认的第二个参数是false，这个是controller的注释，记得修改
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +58,14 @@ namespace LL.FirstCore
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            #region swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelper v1");
+            });
+            #endregion
 
             app.UseRouting();
 
